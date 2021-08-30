92fd3f49f15932de480df5c037a99ad643dd17d5
Neeteesh Kumar <neeteesh.kumar@zs.com>
2021-08-30 12:37:53 +0530

    Routing issue

--- src/App.js
+++ src/App.js
@@ -33,6 +33,7 @@ function App() {
         <ScrollToTop />
         <Switch>
           <Route path= "/" exact component={Home} />
+          <Route path="/myportfolio" exact component={Home} />
           <Route path="/project" component={Projects} />
           <Route path="/about" component={About} />
           <Route path="/resume" component={Resume} />
