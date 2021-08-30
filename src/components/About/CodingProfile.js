c17337b7ae03fff8781b597b5229d124f2505b0d
Neeteesh Kumar <neeteesh.kumar@zs.com>
2021-08-30 14:40:13 +0530

    dynamically fetching data for all coding profiles

--- src/components/About/CodingProfile.js
+++ src/components/About/CodingProfile.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { Col, Row } from "react-bootstrap";
-import Codeforces from "codeforces-api";
 import axios from "axios";
 import {
   SiLeetcode,
@@ -10,13 +9,42 @@ import {
 } from "react-icons/si";
 
 function CodingProfile() {
-  const uri = "https://codeforces.com/api/user.rating?handle=neeteesh205";
+  const codechefUri =
+    "https://competitive-coding-api.herokuapp.com/api/codechef/neeteesh_321";
+  const leetcodeUri =
+    "https://competitive-coding-api.herokuapp.com/api/leetcode/neeteesh205";
+  const codeforcesUri =
+    "https://competitive-coding-api.herokuapp.com/api/codeforces/neeteesh205";
+
   const [codeforcesRating, upadteCodeforcesRating] = useState(0);
+  const [codeChefRating, upadteCodeChefRating] = useState(0);
+  const [leetCodeRating, upadteLeetCodeRating] = useState(0);
+
   useEffect(() => {
     axios
-      .get(uri)
+      .get(codeforcesUri)
       .then((res) => {
-        upadteCodeforcesRating(res.data.result[res.data.result.length-1].newRating);
+        upadteCodeforcesRating(res.data.["max rating"]);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    axios
+      .get(codechefUri)
+      .then((res) => {
+        console.log(res);
+        upadteCodeChefRating(res.data.highest_rating);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    axios
+      .get(leetcodeUri)
+      .then((res) => {
+        console.log(res);
+        upadteLeetCodeRating(res.data.total_problems_solved);
       })
       .catch((err) => {
         console.log(err);
@@ -25,51 +53,54 @@ function CodingProfile() {
 
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <a
-        style={{ textDecoration: "none" }}
-        target="_blank"
-        rel="noopener noreferrer"
-        href="https://leetcode.com/neeteesh205/"
-      >
-        <Col xs={4} md={2} className="tech-icons">
-          <SiLeetcode />
+      <Col xs={4} md={2} className="tech-icons">
+        <SiLeetcode />
+        <a
+          style={{ textDecoration: "none" }}
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://leetcode.com/neeteesh205/"
+        >
           <h5>Leetcode</h5>
-        </Col>
-      </a>
-      <a
-        style={{ textDecoration: "none" }}
-        target="_blank"
-        rel="noopener noreferrer"
-        href="https://codeforces.com/profile/neeteesh205"
-      >
-        <Col xs={4} md={2} className="tech-icons">
-          <SiCodeforces />
+          <h5>Problem Solved : {leetCodeRating}</h5>
+        </a>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiCodeforces />
+        <a
+          style={{ textDecoration: "none" }}
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://codeforces.com/profile/neeteesh205"
+        >
           <h5>Codeforces</h5>
           <h5>Rating : {codeforcesRating}</h5>
-        </Col>
-      </a>
-      <a
-        style={{ textDecoration: "none" }}
-        target="_blank"
-        rel="noopener noreferrer"
-        href="https://www.codechef.com/users/neeteesh_321"
-      >
-        <Col xs={4} md={2} className="tech-icons">
-          <SiCodechef />
+        </a>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiCodechef />
+        <a
+          style={{ textDecoration: "none" }}
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.codechef.com/users/neeteesh_321"
+        >
           <h5>Codechef</h5>
-        </Col>
-      </a>
-      <a
-        style={{ textDecoration: "none" }}
-        target="_blank"
-        rel="noopener noreferrer"
-        href="https://www.hackerrank.com/neeteesh205"
-      >
-        <Col xs={4} md={2} className="tech-icons">
-          <SiHackerrank />
+          <h5>Rating : {codeChefRating}</h5>
+        </a>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiHackerrank />
+        <a
+          style={{ textDecoration: "none" }}
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.hackerrank.com/neeteesh205"
+        >
           <h5>Hackerrank</h5>
-        </Col>
-      </a>
+          <h5>🌟🌟🌟🌟🌟🌟</h5>
+        </a>
+      </Col>
     </Row>
   );
 }
